4a08d0834a4285626f3e515a15691f3ae2a1c9c1
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-09-17 18:51:00 +0200

    new update after mvn test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trying to push my resp");
+        System.out.print("Hello World"); // inte "Hello World!"
     }
 }
